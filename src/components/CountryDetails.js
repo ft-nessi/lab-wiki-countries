0fc3b906b077c6ce0117ea57e7c84e0636e6c541
Vanessa <hello@vfernandes.dev>
2022-03-31 20:34:15 +0200

    Iteration 4

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const url = 'https://ih-countries-api.herokuapp.com/';
+const url = 'https://ih-countries-api.herokuapp.com/countries';
 
 function CountryDetails() {
   const params = useParams();
@@ -17,15 +17,20 @@ function CountryDetails() {
       // const dataList = await res.json();
       // console.log(dataList);
       // setData(dataList);
-      const dataList = await axios.get(url);
-      console.log(dataList);
-      setData(dataList);
+      const {data} = await axios.get(url);
+      setData(data);
     }
     fetchCountries();
   }, []);
+  console.log(data);
   const countryObject = data.find((elem) => {
     return elem.alpha3Code === params.alpha3Code;
   });
+
+  if (!countryObject) {
+    return null;
+  }
+
   return (
     <>
       <div>CountryDetails</div>
@@ -35,6 +40,14 @@ function CountryDetails() {
         src={urlImage + countryObject.alpha2Code.toLowerCase() + '.png'}
         alt="Flag of country"
       ></img>
+      <h1>{countryObject.area}</h1>
+      <h1>{countryObject.borders.map((elem1) => {
+        const countryName = data.filter((elem2) => {
+          return elem2.alpha3Code === elem1
+        })
+        console.log(countryName);
+        return <h3 key={countryName[0].alpha3Code}><Link to={`/${countryName[0].alpha3Code}`}>{countryName[0].name.common}</Link></h3>
+      })}</h1>
     </>
   );
 }
